6ed09314344aaafe14ee01691bbe4306f164b5c0
RicardoWorf <steve4137@gmail.com>
2020-04-30 21:51:36 -0300

    Add feature for pages

--- backend/src/controllers/NewsController.js
+++ backend/src/controllers/NewsController.js
@@ -3,16 +3,51 @@ const axios = require('axios');
 
 module.exports = {
 	async index(req, res) {
+		const { page } = req.query;
 		const { country } = req.body;
 
-		const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.SECRET_KEY}`);
+		const response = await axios.get(
+			'http://newsapi.org/v2/top-headlines?' + 
+			`country=${country}&` +
+			`page=${page}&` +
+			`apiKey=${process.env.SECRET_KEY}`
+		);
 
 		if(response) {
 
-			return res.json(response.data.articles);
+			const news = response.data.articles;
+			const countNews = news.length;
+
+			if(countNews !== 0 ) {
+				return res.json({ count: countNews, news });	
+			}
+
+			return res.json({ page: 'Not exist more pages' });
+			
+		}
+
+		return res.status(402).json({ error: 'Error in source news, try another.'});
+	},
+
+	async show(req, res) {
+		const { page } = req.query;
+		const { source } = req.body;
+
+		const response = await axios.get(
+			'http://newsapi.org/v2/top-headlines?' +
+			 `sources=${source}&` + 
+			 `page=${page === '' ? 0 : page}&` +
+			 `apiKey=${process.env.SECRET_KEY}`
+		);
+
+		if(response) {
+
+			return res.json(response.data);
 			
 		}
 
 		return res.status(402).json({ error: 'Error in source news, try another.'});
 	}
+
+
 }
