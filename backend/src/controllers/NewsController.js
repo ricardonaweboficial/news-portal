85753c0aecd95a27f837eae5157377574087d975
RicardoWorf <steve4137@gmail.com>
2020-05-05 21:11:35 -0300

    Fix response data of API

--- backend/src/controllers/NewsController.js
+++ backend/src/controllers/NewsController.js
@@ -3,26 +3,18 @@ const axios = require('axios');
 
 module.exports = {
 	async index(req, res) {
-		const { page } = req.query;
-		const { country } = req.body;
 
 		const response = await axios.get(
 			'http://newsapi.org/v2/top-headlines?' + 
-			`country=${country}&` +
-			`page=${page}&` +
+			`country=br&` +
 			`apiKey=${process.env.SECRET_KEY}`
 		);
 
 		if(response) {
 
 			const news = response.data.articles;
-			const countNews = news.length;
 
-			if(countNews !== 0 ) {
-				return res.json({ count: countNews, news });	
-			}
-
-			return res.json({ page: 'Not exist more pages' });
+			return res.json(news);
 			
 		}
 
@@ -30,19 +22,18 @@ module.exports = {
 	},
 
 	async show(req, res) {
-		const { page } = req.query;
 		const { source } = req.body;
 
 		const response = await axios.get(
 			'http://newsapi.org/v2/top-headlines?' +
 			 `sources=${source}&` + 
-			 `page=${page === '' ? 0 : page}&` +
 			 `apiKey=${process.env.SECRET_KEY}`
 		);
 
 		if(response) {
-
-			return res.json(response.data);
+			const news = response.data.articles;
+			
+			return res.json(news);
 			
 		}
 
