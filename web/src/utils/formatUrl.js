3469d7ec10979ae14478b536ed44ecf5b1655d91
RicardoWorf <steve4137@gmail.com>
2020-05-19 17:37:02 -0300

    feat: Improve view url

--- web/src/utils/formatUrl.js
+++ web/src/utils/formatUrl.js
@@ -1,6 +1,15 @@
 export default function formatUrl(url) {// Clear Url for view creater
-	const removeHttps = url.slice(8,url.length - 8);
-	const limitUrl = removeHttps.indexOf('/');
-	const urlClear = removeHttps.slice(0, limitUrl)
-	return urlClear
+	const https = url.slice(0,5)
+	if(https === 'https') {
+		const removeHttps = url.slice(8,url.length - 8);
+		const limitUrl = removeHttps.indexOf('/');
+		const urlClear = removeHttps.slice(0, limitUrl)
+		return urlClear	
+	} else {
+		const removeHttps = url.slice(7,url.length - 7);
+		const limitUrl = removeHttps.indexOf('/');
+		const urlClear = removeHttps.slice(0, limitUrl)
+		return urlClear	
+	}
+	
 }
